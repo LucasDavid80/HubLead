207e021907ce43b25ea85745dda42a46d36929d5
Lucas David <lucasdavidoliveira80@gmail.com>
2026-01-28 17:53:56 -0300

    feat: atualizar configuração do Firebase para usar variáveis de ambiente

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBImke3vEFE0_VLv868iZ6rl-WC5WDGKnQ",
-    authDomain: "hublead-mvp.firebaseapp.com",
-    projectId: "hublead-mvp",
-    storageBucket: "hublead-mvp.firebasestorage.app",
-    messagingSenderId: "315640575902",
-    appId: "1:315640575902:web:4953f7293c4822e1d9888f"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Singleton para iniciar o Firebase apenas uma vez
